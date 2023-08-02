63ff7b005ba3fccfa556a5f9276c3db82b614690
Sowmya Kari <karisowmya70@gmail.com>
2023-08-02 13:08:07 -0400

    New changes

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-const imagecontainerE1 = document.querySelector(".imagecontainerE1");
+const imageContainerE1 = document.querySelector(".image-container");
 const btnE1 = document.querySelector(".btn");
-btnE1.addEventListener("click", () => { 
+ btnE1.addEventListener("click", () => { 
     imageNum = 10; 
     addNewImages()
+    
 });
 function addNewImages(){
     for(let index =0; index < imageNum;index++)
     {
         const newImaE1 = document.createElement("img")
         newImaE1.src = `https://picsum.photos/300?random=${Math.floor(Math.random()*2000)}`;
-        imagecontainerE1.appendChild(newImaE1);
+        imageContainerE1.appendChild(newImaE1);
     }
-    
 
+    
 }
